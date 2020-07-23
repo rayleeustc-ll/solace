807f2c8e72d4124fac973cacb2c7d331396a3c5d
rayleeustc-ll <691467078@qq.com>
2020-07-23 16:52:42 +0800

    commit for topic p/s

---
+++ solace_smp_mine/src/main/java/TopicPublisher.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import com.solacesystems.jcsmp.JCSMPException;
+import com.solacesystems.jcsmp.JCSMPFactory;
+import com.solacesystems.jcsmp.JCSMPProperties;
+import com.solacesystems.jcsmp.JCSMPSession;
+import com.solacesystems.jcsmp.JCSMPStreamingPublishEventHandler;
+import com.solacesystems.jcsmp.TextMessage;
+import com.solacesystems.jcsmp.Topic;
+import com.solacesystems.jcsmp.XMLMessageProducer;
+
+public class TopicPublisher {
+
+    public static void main(String... args) throws JCSMPException {
+
+        // Check command line arguments
+
+
+        System.out.println("TopicPublisher initializing...");
+
+        // Create a JCSMP Session
+        final JCSMPProperties properties = new JCSMPProperties();
+        properties.setProperty(JCSMPProperties.HOST,"szhpc6287");     // host:port
+        properties.setProperty(JCSMPProperties.USERNAME, "lelewang"); // client-username
+        properties.setProperty(JCSMPProperties.VPN_NAME,  "default"); // message-vpn
+        properties.setProperty(JCSMPProperties.PASSWORD, "lelewang"); // client-password
+
+        final JCSMPSession session =  JCSMPFactory.onlyInstance().createSession(properties);
+
+        session.connect();
+
+        final Topic topic = JCSMPFactory.onlyInstance().createTopic("tutorial/topic");
+
+        /** Anonymous inner-class for handling publishing events */
+        XMLMessageProducer prod = session.getMessageProducer(new JCSMPStreamingPublishEventHandler() {
+
+            public void responseReceived(String messageID) {
+                System.out.println("Producer received response for msg: " + messageID);
+            }
+            public void handleError(String messageID, JCSMPException e, long timestamp) {
+                System.out.printf("Producer received error for msg: %s@%s - %s%n",
+                        messageID,timestamp,e);
+            }
+        });
+        // Publish-only session is now hooked up and running!
+
+        TextMessage msg = JCSMPFactory.onlyInstance().createMessage(TextMessage.class);
+        final String text = "Hello world!";
+        msg.setText(text);
+        System.out.printf("Connected. About to send message '%s' to topic '%s'...%n",text,topic.getName());
+        prod.send(msg,topic);
+        System.out.println("Message sent. Exiting.");
+        session.closeSession();
+    }
+}
